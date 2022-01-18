54e99e0d987f4f525c8d03d48373ab6859436c56
Sneha-M07 <sneha.m@moonraft.com>
2022-01-18 03:37:58 -0800

    updated on servlet

--- core/src/main/java/com/moonraft/core/servlets/TestSlingServlet.java
+++ core/src/main/java/com/moonraft/core/servlets/TestSlingServlet.java
@@ -9,7 +9,6 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.apache.sling.servlets.annotations.SlingServletPaths;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
@@ -31,24 +30,27 @@ public class TestSlingServlet extends SlingSafeMethodsServlet{
     protected void doGet(final SlingHttpServletRequest req, final SlingHttpServletResponse resp) throws ServletException, IOException {
         final ResourceResolver resourceResolver = req.getResourceResolver();
         String callingPage = req.getParameter("callingPage");
-        if(callingPage!=null){
+        int pageDepth = Integer.parseInt(req.getParameter("depth"));
+        if(callingPage!=null && pageDepth>0){
         	Page page = resourceResolver.adaptTo(PageManager.class).getPage(callingPage);
             JSONArray pagesArray = new JSONArray();
         try {
            JSONObject pageObject1 = new JSONObject();
     	   pageObject1.put("Parent Page Title",page.getTitle().toString());
     	   pageObject1.put("Parent Page Template Type",page.getTemplate().getProperties().get("cq:templateType", "Default template type"));
-           pagesArray.put(pageObject1);
+    	   pagesArray.put(pageObject1);
             Iterator<Page> childPages = page.listChildren(null, true);
             while (childPages.hasNext()) {
-                Page childPage = childPages.next();
+            	Page childPage = childPages.next();
                 JSONObject pageObject = new JSONObject();
-                pageObject.put("Page Title",childPage.getTitle().toString());
+            	if(childPage.getDepth()<=page.getDepth()+pageDepth) {
+               // pageObject.put("Parent Page",childPage.getParent().getPath());
+                pageObject.put("Page Title",childPage.getTitle());
                // pageObject.put("Template path",childPage.getTemplate().getPath().toString());
                // pageObject.put("Template",childPage.getTemplate().getProperties().get(JcrConstants.JCR_TITLE));
                 pageObject.put("Template Type",childPage.getTemplate().getProperties().get("cq:templateType", "Default template type"));
                 pagesArray.put(pageObject);
-               
+            	}
             }
             resp.setContentType("application/json");
             resp.getWriter().write(pagesArray.toString());
